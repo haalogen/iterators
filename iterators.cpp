4cdf35d299650893b7242888ea6a64cb1d8061c3
stan <citrus_82@nextmail.ru>
2015-04-04 00:24:57 +0300

    Defined class FwdIter

--- iterators.cpp
+++ iterators.cpp
@@ -1,69 +1,64 @@
 #include <iostream>
 using namespace std;
 
-template <class T> class ForwardIterator {
-private:
-    T *p; // [0;size-1]
-    size_t size;
-    T *first;
-    T *last; // &p[size-1];
+template<class T> class Fwd_iter {
+protected:
+    T *it;
 public:
-    ForwardIterator (const T* w, int sz = 32) {
-        cout << "Constructor \n"; 
-        size = sz;
-        p = new T[size];
-        first = &p[0];
-        last = &p[size-1];
-        
-        T* tmp = w;
-        for (p = first; p != last; ++p) {
-            *p = *w;
-            cout << *p << endl;
-            ++w;
-        }
-    }
-    ForwardIterator (const ForwardIterator &it) {
-        cout << "Copy constructor \n"; 
-        size = it.size;
-        p = new T[size];
-        first = &p[0];
-        last = &p[size-1];
-    }
-    ForwardIterator operator++ () {
-        cout << "Pre-increment \n";
-        T *tmp;
-        ++tmp;
-        if (tmp == last) cerr << "[!]Out of bounds! \n";
-        else ++p;
-        return *this;
-    }
-    inline void printAddr() {
-        cout << "Iterator address: " << p << endl;
-    }
-    bool onLast()
-    {
-        cout << "OnLast check \n";
-        if (p == last) return true;
-        return false;
-    }
-    ~ForwardIterator(){ cout << "Destructor \n"; delete [] first; }
+    Fwd_iter (T *c) { cout << "Fwd_iter constructor \n"; it = c; }
 
+    Fwd_iter (Fwd_iter &t) { it = t.it; }
+
+    ~Fwd_iter () { cout << get_addr() << " Fwd_iter destructor \n"; }
+
+    inline int* get_addr() { return it; }
+    
+    T& operator* () { return *it; }
+    
+    T* operator++ () { ++it; return it; }
 };
 
 
 
 int main()
 {
-
-    const int sz = 10;
-    int *q = new int[sz];
-
-    ForwardIterator it(q, sz);
-
-    it.printAddr();
-    ++it;
-    it.printAddr();
- //   ForwardIterator<int> a(it);
-    cout << sizeof(size_t) << endl;
+    cout << "Hello" << endl;
+    register int i = 0;
+    int a[30];
+    cout << a << endl;
     
+    for (i=0;i<30;++i)
+    {
+        a[i] = i;
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    
+    Fwd_iter<int> itfwd_begin(a);
+
+    cout << itfwd_begin.get_addr() << endl;
+    
+    Fwd_iter<int> itfwd(a);
+    cout << itfwd.get_addr() << endl;
+    
+    for (i=0;i<10;++i) {
+        cout << *itfwd << " ";
+        ++itfwd;
+    }
+    cout << "\n\n";
+        
+//    Bid_iter<int> it(itfwd, itfwd_begin);
+//    
+//    for (i=0;i<5;++i) {
+//        cout << *it << " ";
+//        ++it;
+//    }
+//    cout << "\n\n";
+//    
+//    for (i=0;i<5;++i) {
+//        cout << *it << " ";
+//        --it;
+//    }
 }
+
+
